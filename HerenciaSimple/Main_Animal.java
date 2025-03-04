158b1437eb9c5e9b7ebaf70eb81e9284767ae53b
nrojo258 <neroce@prateducacio.com>
2025-03-04 10:35:10 +0100

    Add files via upload

---
+++ HerenciaSimple/Main_Animal.java
@@ -0,0 +1,28 @@
+package HerenciaSimple;
+
+public class Main_Animal {
+    public static void main(String[] args) {
+        //animal
+        Animal a = new Animal();
+        //animal crida la funció sound();
+        a.sound();
+
+        Animal a1 = new Animal();
+        //animal crida la funció sound();
+        a1.sound();
+
+        //dog
+        Dog d = new Dog();
+        //dog crida la funció sound();
+        d.sound();
+
+        //dog
+        Dog d1 = new Dog();
+        //dog crida la funció sound();
+        d1.sound();
+
+        Dog d2 = new Dog(23);
+        //dog crida la funció sound();
+        d2.sound();
+    }
+}
